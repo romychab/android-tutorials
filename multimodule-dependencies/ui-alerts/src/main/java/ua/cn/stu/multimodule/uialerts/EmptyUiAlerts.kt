1b70baa401303cc22db0846b1a7935f3ef6aa73a
Roman Andrushchenko <rom.andrushchenko@gmail.com>
2023-01-26 09:47:16 +0200

    Minor fixes

--- multimodule-dependencies/ui-alerts/src/main/java/ua/cn/stu/multimodule/uialerts/EmptyUiAlerts.kt
+++ multimodule-dependencies/ui-alerts/src/main/java/ua/cn/stu/multimodule/uialerts/EmptyUiAlerts.kt
@@ -1,5 +1,9 @@
 package ua.cn.stu.multimodule.uialerts
 
+/**
+ * Empty implementation is used in case if the real implementation
+ * hasn't been added / can't be found.
+ */
 class EmptyUiAlerts : UiAlerts {
 
     override fun toast(message: String) {
